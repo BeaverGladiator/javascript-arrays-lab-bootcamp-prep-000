84f3531347f8435723f3bba99b3bde1adbd182e7
Matthew Migliore <mmigliore311@gmail.com>
2018-08-17 01:06:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -32,6 +32,6 @@ function prependKitten(name) {
 }
 
 function removeLastKitten() {
-  var newKittens = kittens.pop()
-  newKittens
+  var newKittens = kittens.slice(1, kitten.length -1)
+  return newKittens
 }
